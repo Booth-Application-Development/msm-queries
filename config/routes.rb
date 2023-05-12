c5747d26b42a3eaff466ff63ec34eba6028a555e
Harminder Singh <harminder.singh@chicagobooth.edu>
2023-05-12 21:59:04 +0000

    Save1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
+
+get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+get("/directors/:an_id", { :controller => "directord", :action => "directors_details"})
+
+
+get("/", { :controller => "application", :action => "homepage" })
+
+get("/directors", { :controller => "directors", :action => "index" })
+
 end
